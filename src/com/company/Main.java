615c45085560422275f40cef54502770a1f2b8dc
SSHUSSAIN-86 <shehzad.hussain2011@gmail.com>
2021-08-25 18:12:17 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	// write your code here
+        System.out.println("Hello Nika!!"); //Always put a semicolon at the end of each statement
+
+        /* This is another way to write your comments in your program
+         My comments second line
+         My comments third line */
+    }
+}
